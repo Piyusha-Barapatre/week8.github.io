ed48b013e159836cdf0364153575d1a32ad2bca8
Piyusha-Barapatre <85167734+Piyusha-Barapatre@users.noreply.github.com>
2022-04-27 23:12:27 +0530

    Update game.php

--- game.php
+++ game.php
@@ -45,7 +45,7 @@ $result = check($computer, $human);
 <!DOCTYPE html>
 <html>
 <head>
-<title>Anamitra Musib 32ce3337</title>
+<title> Piyusha Barapatre 1da6a3b3 </title>
 <?php require_once "bootstrap.php"; ?>
 </head>
 <body>
